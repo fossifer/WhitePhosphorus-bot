67a4bde54645fe032363ea213686fd3c313c97ea
lziad <daizl@pku.edu.cn>
2017-03-03 16:29:11 +0800

    fix some bugs and will put {{d|g1}} when the page is empty

--- src/badimage.py
+++ src/badimage.py
@@ -10,6 +10,7 @@ from botsite import cur_timestamp
 bad_re = re.compile(r'{{\s*(受限制文件' \
                     r'|[Rr]estricted use|[Bb]ad ?image)\s*}}')
 file_re = re.compile(r'\[\[:?[Ff]ile:(?P<name>.*?)\]\]')
+empty_re = re.compile(r'\s*')
 site = botsite.Site()
 bad_list = []
 
@@ -31,28 +32,31 @@ def check_image(title):
 def add():
     for image in bad_list:
         if check_image(image):
-            image = 'File:Ejaculation educational ani short.gif'
             image_text = site.get_text_by_title(image)
             if bad_re.search(image_text):
                 continue
-            new_text = '{{受限制文件}}' if image_text else '\n{{受限制文件}}'
+            new_text = '\n{{受限制文件}}' if image_text else '{{受限制文件}}'
             site.edit(new_text,
                       '机器人：为[[MediaWiki:Bad image list]]中的文件添加' \
                       '{{[[Template:受限制文件|受限制文件]]}}', bot=True,
                       title=image, nocreate=False, append=True,
-                      basets=site.ts, startts=site.ts, print_only=True)
+                      basets=site.ts, startts=site.ts)
 
 
 def remove():
     for title in site.what_embeds_it(
             title='Template:受限制文件', ns='6', id=False):
         if title not in bad_list:
+            t = ''
             new_text = bad_re.sub('', site.get_text_by_title(image))
+            if not empty_re.sub('', new_text):
+                new_text = '{{d|g1}}'
+                t = '并提请快速删除（G1）'
             site.edit(new_text,
-                      '机器人：%s不在[[MediaWiki:Bad image list]]中，移除' \
-                      '{{[[Template:受限制文件|受限制文件]]}}',
+                      '机器人：[[%s]]不在[[MediaWiki:Bad image list]]中，移除' \
+                      '{{[[Template:受限制文件|受限制文件]]}}%s' % (title, t),
                       bot=True, small=True, title=title,
-                      basets=site.ts, startts=site.ts, print_only=True)
+                      basets=site.ts, startts=site.ts)
 
 
 if __name__ == '__main__':
